321f7226bff1ce010469a825df3f7abf4de0f4f7
syk2018 <syk2018@gmail.com>
2019-07-29 13:57:07 +0300

    Create primary pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
+  { path: 'tabs', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
+  { path: 'aboutme', loadChildren: './aboutme/aboutme.module#AboutmePageModule' },
+  { path: 'aboutme', loadChildren: './pages/aboutme/aboutme.module#AboutmePageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
 ];
 
 @NgModule({
